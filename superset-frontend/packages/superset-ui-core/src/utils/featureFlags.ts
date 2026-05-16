cddd431759382d7c4bd7b50e3e6e1c5d5b618f07
马泽伟 <zewe@mazeweideMini.lan>
2026-05-16 14:06:32 +0800

    feat: add column view scheme flags

--- superset-frontend/packages/superset-ui-core/src/utils/featureFlags.ts
+++ superset-frontend/packages/superset-ui-core/src/utils/featureFlags.ts
@@ -29,6 +29,7 @@ export enum FeatureFlag {
   AllowFullCsvExport = 'ALLOW_FULL_CSV_EXPORT',
   AvoidColorsCollision = 'AVOID_COLORS_COLLISION',
   ChartPluginsExperimental = 'CHART_PLUGINS_EXPERIMENTAL',
+  ColumnViewSchemeEnabled = 'COLUMN_VIEW_SCHEME_ENABLED',
   ConfirmDashboardDiff = 'CONFIRM_DASHBOARD_DIFF',
   CssTemplates = 'CSS_TEMPLATES',
   DashboardVirtualization = 'DASHBOARD_VIRTUALIZATION',
--- superset/config.py
+++ superset/config.py
@@ -534,6 +534,7 @@ DEFAULT_FEATURE_FLAGS: dict[str, bool] = {
     "ALERT_REPORTS": False,
     "ALERT_REPORT_TABS": False,
     "ALERT_REPORT_SLACK_V2": False,
+    "COLUMN_VIEW_SCHEME_ENABLED": False,
     "DASHBOARD_RBAC": False,
     "ENABLE_ADVANCED_DATA_TYPES": False,
     # Enabling ALERTS_ATTACH_REPORTS, the system sends email and slack message
